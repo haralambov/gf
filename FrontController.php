a652b84c84b976754d362be7530cd0bc4d62af60
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-07 22:27:43 +0300

    Adds casing consistency updates for controllers and methods
    This is the code from lesson #20.
    
    Youtube link: https://www.youtube.com/watch?v=kTUNkiPyQ-s
    Website link:
    https://nau4i.me/php/php-mvc-framework/378/mvc-framework-20-front-controller-%D1%80%D1%83%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%B55

--- FrontController.php
+++ FrontController.php
@@ -49,10 +49,10 @@ class FrontController
 
         $_params = explode('/', $_uri);
         if ($_params[0]) {
-            $this->controller = $_params[0];
+            $this->controller = strtolower($_params[0]);
             // if we do not have controller and method, we do not have params
             if ($_params[1]) {
-                $this->method = $_params[1];
+                $this->method = strtolower($_params[1]);
             } else {
                 $this->method = $this->getDefaultMethod();
             }
@@ -61,14 +61,16 @@ class FrontController
             $this->method = $this->getDefaultMethod();
         }
 
-        if (is_array($_rc) && $_rc['controllers'] && $_rc['controllers'][$this->controller]['to']) {
+        if (is_array($_rc) && $_rc['controllers']) {
             if ($_rc['controllers'][$this->controller]['methods'][$this->method]) {
-                $this->method = $_rc['controllers'][$this->controller]['methods'][$this->method];
+                $this->method = strtolower($_rc['controllers'][$this->controller]['methods'][$this->method]);
             }
-            $this->controller = $_rc['controllers'][$this->controller]['to'];
+            if (isset($_rc['controllers'][$this->controller]['to'])) {
+                $this->controller = strtolower($_rc['controllers'][$this->controller]['to']);
+            }
         }
 
-        $f = $this->ns . '\\' . $this->controller;
+        $f = $this->ns . '\\' . ucfirst($this->controller);
         $newController = new $f();
         $newController->{$this->method}();
     }
@@ -76,15 +78,15 @@ class FrontController
     public function getDefaultController() {
         $controller = \GF\App::getInstance()->getConfig()->app['default_controller'];
         if ($controller) {
-            return $controller;
+            return strtolower($controller);
         }
-        return 'Index';
+        return 'index';
     }
 
     public function getDefaultMethod() {
         $method = \GF\App::getInstance()->getConfig()->app['default_method'];
         if ($method) {
-            return $method;
+            return strtolower($method);
         }
         return 'index';
     }
