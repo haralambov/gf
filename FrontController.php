166ebbafcc89f24724f317dc5e5c85548effde92
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-07 19:35:06 +0300

    Adds extracting the namespace from the config
    This is the code from lesson #17.
    
    Youtube link: https://www.youtube.com/watch?v=T-Mpa2A_EsY
    Website link:
    https://nau4i.me/php/php-mvc-framework/375/mvc-framework-17-front-controller-%D1%80%D1%83%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%B52

--- FrontController.php
+++ FrontController.php
@@ -5,6 +5,9 @@ namespace GF;
 class FrontController
 {
     private static $_instance = null;
+    private $ns = null;
+    private $controller = null;
+    private $method = null;
 
     private function __construct() {
     }
@@ -23,6 +26,24 @@ class FrontController
         $router = new \GF\Routers\DefaultRouter();
         $_uri = $router->getURI();
         $routes = \GF\App::getInstance()->getConfig()->routes;
+        if (is_array($routes) && count($routes) > 0) {
+            foreach ($routes as $k => $v) {
+                if (stripos($_uri, $k) === 0 && $v['namespace']) {
+                    $this->ns = $v['namespace'];
+                    break;
+                }
+            }
+        } else {
+            throw new \Exception('Default route missing', 500);
+        }
+
+        if ($this->ns == null && $routes['*']['namespace']) {
+            $this->ns = $routes['*']['namespace'];
+        } else if ($this->ns == null && !$routes['*']['namespace']) {
+            throw new \Exception('Default route missing', 500);
+        }
+
+        echo $this->ns;
     }
 
     public function getDefaultController() {
