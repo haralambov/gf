535700a9b00be15263e7768f2aa3a0626b2f9f43
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-07 13:34:09 +0300

    Updates DefaultRouter parse method
    This is the code from lesson #11.
    
    Youtube link: https://www.youtube.com/watch?v=TKpjCsJku2g
    Website link:
    https://nau4i.me/php/php-mvc-framework/369/mvc-framework-11-default-router3

--- Routers/DefaultRouter.php
+++ Routers/DefaultRouter.php
@@ -4,25 +4,34 @@ namespace GF\Routers;
 
 class DefaultRouter
 {
+    private $controller = null;
+    private $method = null;
+    private $params = array();
+
     public function parse() {
-        $_uri = substr(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PHP_SELF']), 1);
-        $controller = null;
-        $method = null;
+        $_uri = substr($_SERVER['SCRIPT_NAME'], strlen($_SERVER['PHP_SELF']) + 1);
         $_params = explode('/', $_uri);
         if ($_params[0]) {
-            $controller .= ucfirst($_params[0]);
+            $this->controller = ucfirst($_params[0]);
 
             // if we do not have controller and method, we do not have params
             if ($_params[1]) {
-                $method = $_params[1];
+                $this->method = $_params[1];
                 unset($_params[0], $_params[1]);
-            } else {
-                $method = 'index';
+                $this->params = array_values($_params);
             }
-        } else {
-            $controller = 'index';
-            $method = 'index';
         }
-        echo $controller . '<br>' . $method;
+    }
+
+    public function getController() {
+        return $this->controller;
+    }
+
+    public function getMethod() {
+        return $this->method;
+    }
+
+    public function getGet() {
+        return $this->params;
     }
 }
