b0a62894a02a018fa11d738883dd3729d1f55e74
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-07 20:02:10 +0300

    Expands the routing logic and extracting the controller from the config
    This is the code from lesson #18.
    
    Youtube link: https://www.youtube.com/watch?v=ZSshT_r9ihY
    Website link:
    https://nau4i.me/php/php-mvc-framework/376/mvc-framework-18-front-controller-%D1%80%D1%83%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%B53

--- FrontController.php
+++ FrontController.php
@@ -26,10 +26,13 @@ class FrontController
         $router = new \GF\Routers\DefaultRouter();
         $_uri = $router->getURI();
         $routes = \GF\App::getInstance()->getConfig()->routes;
+        $_rc = null;
         if (is_array($routes) && count($routes) > 0) {
             foreach ($routes as $k => $v) {
-                if (stripos($_uri, $k) === 0 && $v['namespace']) {
+                if (stripos($_uri, $k) === 0 && ($_uri == $k || stripos($_uri, $k . '/') === 0) && $v['namespace']) {
                     $this->ns = $v['namespace'];
+                    $_uri = substr($_uri, strlen($k) + 1);
+                    $_rc = $v;
                     break;
                 }
             }
@@ -39,11 +42,32 @@ class FrontController
 
         if ($this->ns == null && $routes['*']['namespace']) {
             $this->ns = $routes['*']['namespace'];
+            $_rc = $routes['*'];
         } else if ($this->ns == null && !$routes['*']['namespace']) {
             throw new \Exception('Default route missing', 500);
         }
 
-        echo $this->ns;
+        $_params = explode('/', $_uri);
+        if ($_params[0]) {
+            $this->controller = $_params[0];
+            // if we do not have controller and method, we do not have params
+            if ($_params[1]) {
+                $this->method = $_params[1];
+            } else {
+                $this->method = $this->getDefaultMethod();
+            }
+        } else {
+            $this->controller = $this->getDefaultController();
+            $this->method = $this->getDefaultMethod();
+        }
+
+        if (is_array($_rc) && $_rc['controllers'] && $_rc['controllers'][$this->controller]) {
+            $this->controller = $_rc['controllers'][$this->controller];
+        }
+
+        echo $this->ns . '<br>';
+        echo $this->controller . '<br>';
+        echo $this->method . '<br>';
     }
 
     public function getDefaultController() {
