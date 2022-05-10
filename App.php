07e18b2846f095773315bbe75373052093be8757
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-10 20:34:27 +0300

    Adds SimpleDB class
    This is the code from lesson #27.
    
    Youtube link: https://www.youtube.com/watch?v=inFAIsLuaR0
    Website link:
    https://nau4i.me/php/php-mvc-framework/385/mvc-framework-27-database4

--- App.php
+++ App.php
@@ -66,7 +66,7 @@ class App
         $this->_frontController->dispatch();
     }
 
-    public function getConnection($connection = 'default') {
+    public function getDBConnection($connection = 'default') {
         if (!$connection) {
             throw new \Exception('No connection identifier provided', 500);
         }
--- DB/SimpleDB.php
+++ DB/SimpleDB.php
@@ -4,4 +4,17 @@ namespace GF\DB;
 
 class SimpleDB {
 
+    private $db = null;
+    protected $connection = 'default';
+
+    public function __construct($connection = null) {
+        if ($connection instanceof \PDO) {
+            $this->db = $connection;
+        } else if ($connection != null) {
+            $this->db = \GF\App::getInstance()->getDBConnection($connection);
+            $this->connection = $connection;
+        } else {
+            $this->db = \GF\App::getInstance()->getDBConnection($this->connection);
+        }
+    }
 }
