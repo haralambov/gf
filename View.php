76b8a2aa0755ad6d5e5e17bb5fec5c91a69b5ccb
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-11 22:59:54 +0300

    Updated View class - trying to avoid naming collisions
    This is the code from lesson #35.
    
    Youtube link: https://www.youtube.com/watch?v=XhqP_9L7wz4
    Website link:
    https://nau4i.me/php/php-mvc-framework/393/mvc-framework-35-views3

--- View.php
+++ View.php
@@ -3,16 +3,16 @@
 namespace GF;
 
 class View {
-    private static $_instance = null;
-    private $viewPath = null;
-    private $viewDir = null;
-    private $data = array();
-    private $extension = '.php';
+    private static $___instance = null;
+    private $___viewPath = null;
+    private $___viewDir = null;
+    private $___data = array();
+    private $___extension = '.php';
 
     private function __construct() {
-        $this->viewPath = \GF\App::getInstance()->getConfig()->app['viewDirectory'];
-        if ($this->viewPath == null) {
-            $this->viewPath = realpath('../views/');
+        $this->___viewPath = \GF\App::getInstance()->getConfig()->app['viewDirectory'];
+        if ($this->___viewPath == null) {
+            $this->___viewPath = realpath('../views/');
         }
     }
 
@@ -21,7 +21,7 @@ class View {
         if ($path) {
             $path = realpath($path) . DIRECTORY_SEPARATOR;
             if (is_dir($path) && is_readable($path)) {
-                $this->viewDir = $path;
+                $this->___viewDir = $path;
             } else {
                 //TODO
                 throw new \Exception('View path', 500);
@@ -34,7 +34,7 @@ class View {
 
     public function display($name, $data = array(), $returnAsString = false) {
         if (is_array($data)) {
-            $this->data = array_merge($this->data, $data);
+            $this->___data = array_merge($this->___data, $data);
         }
 
         if ($returnAsString) {
@@ -45,14 +45,13 @@ class View {
     }
 
     private function _includeFile($file) {
-        if ($this->viewDir == null) {
-            $this->setViewDirectory($this->viewPath);
+        if ($this->___viewDir == null) {
+            $this->setViewDirectory($this->___viewPath);
         }
-        $p = str_replace('.', DIRECTORY_SEPARATOR, $file);
-        $fl = $this->viewDir . $p . $this->extension;
-        if (is_file($fl) && is_readable($fl)) {
+        $___fl = $this->___viewDir . str_replace('.', DIRECTORY_SEPARATOR, $file) . $this->___extension;
+        if (is_file($___fl) && is_readable($___fl)) {
             ob_start();
-            include $fl;
+            include $___fl;
             return ob_get_clean();
         } else {
             throw new \Exception('View ' . $file . ' cannot be included', 500);
@@ -61,18 +60,18 @@ class View {
     }
 
     public function __set($name, $value) {
-        $this->data[$name] = $value;
+        $this->___data[$name] = $value;
     }
 
     public function __get($name) {
-        return $this->data[$name];
+        return $this->___data[$name];
     }
 
     /** @return \GF\View  */
     public function getInstance() {
-        if (self::$_instance == null) {
-            self::$_instance = new \GF\View();
+        if (self::$___instance == null) {
+            self::$___instance = new \GF\View();
         }
-        return self::$_instance;
+        return self::$___instance;
     }
 }
