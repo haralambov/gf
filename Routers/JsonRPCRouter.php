7023fb8c3e7da27572c928744e155182779d8a3d
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-12 22:02:42 +0300

    Adds JsonRPCRouter
    This is the code from lesson #38.
    
    Youtube link: https://www.youtube.com/watch?v=mdj3tyvGAP4
    Website link:
    https://nau4i.me/php/php-mvc-framework/396/mvc-framework-38-rpcrouter1

---
+++ Routers/JsonRPCRouter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace GF\Routers;
+
+class JsonRPCRouter implements \GF\Routers\IRouter {
+
+    public function __construct() {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST' || empty($_SERVER['CONTENT_TYPE']) || $_SERVER['CONTENT_TYPE'] != 'application/json') {
+            throw new \Exception('Require json request', 400);
+        }
+    }
+
+    public function getURI() { }
+
+}
