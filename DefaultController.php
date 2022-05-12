f011efbfdea49b772963b85926315bd6c4e2a6ca
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-12 23:15:31 +0300

    Adds a default controller
    This is the code from lesson #42.
    
    Youtube link: https://www.youtube.com/watch?v=1coSluN5zPE
    Website link:
    https://nau4i.me/php/php-mvc-framework/400/mvc-framework-42-basecontroller

---
+++ DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GF;
+
+class DefaultController {
+    /**
+     * @var \GF\App
+     */
+    public $app;
+    /**
+     * @var \GF\View
+     */
+    public $view;
+    /**
+     * @var \GF\Config
+     */
+    public $config;
+    /**
+     * @var \GF\InputData
+     */
+    public $input;
+
+    public function __construct() {
+        $this->app = \GF\App::getInstance();
+        $this->view = \GF\View::getInstance();
+        $this->config = $this->app->getConfig();
+        $this->input = \GF\InputData::getInstance();
+    }
+}
